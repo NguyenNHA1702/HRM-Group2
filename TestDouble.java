3ca8a30a96fb9ee53a462d2c6288826d1e0944a7
tungnthhe180442 <tungnthhe180442@fpt.edu.vn>
2026-07-16 03:25:57 +0700

    chore: clean up temporary scripts and compiled class files from root directory

--- TestDouble.java
+++
@@ -1,9 +0,0 @@
-public class TestDouble {
-    public static void main(String[] args) {
-        try {
-            System.out.println(Double.parseDouble("d"));
-        } catch(Exception e) {
-            System.out.println("Exception: " + e.getClass().getName());
-        }
-    }
-}
